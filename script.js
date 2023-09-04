bfbc33fcce177e3df948ba281e8d0d8747799bf1
sumanta kumar mohanta <mohantasumanta601@gmail.com>
2023-09-04 21:10:19 +0530

    script file added

---
+++ script.js
@@ -0,0 +1,11 @@
+// for (let i=0;i<= 10;i++){
+//     console.log("Number "+i);
+// }
+
+for(let i=0; i<= 10;i++){
+    if(i === 7){
+        console.log(`${i} is my lucky number`);
+    }else{
+        console.log(i);
+    }
+}
